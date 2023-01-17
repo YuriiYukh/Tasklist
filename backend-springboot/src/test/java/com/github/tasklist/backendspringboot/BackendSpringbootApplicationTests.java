1be114e927ae8eac8ac321518d592e5ded18b914
Yukhymenko Yuriy <yuhim01@gmail.com>
2023-01-17 22:59:58 +0200

    Initial commit

---
+++ backend-springboot/src/test/java/com/github/tasklist/backendspringboot/BackendSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.tasklist.backendspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
